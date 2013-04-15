6166e70238f6da2e8266ec927eb0a7a7e5de967f
Colleen Murphy <ckrinkle101@gmail.com>
2013-04-15 11:24:46 -0700

    Check for URL already existing in db.

--- translate.rb
+++ translate.rb
@@ -6,7 +6,7 @@ $dbfile = 'lilurl.db'
 
 def geturl(hash)
   urldb = SQLite3::Database.open $dbfile
-  statement = urldb.prepare "SELECT url FROM urls WHERE hashid = ?"
+  statement = urldb.prepare "SELECT url FROM urls WHERE hash = ?"
   statement.bind_param 1, dbstring(hash)
   response = statement.execute
   row = response.next # since hash is a primary key this query should only return one result
@@ -35,7 +35,7 @@ def makeurl(oldurl, postfix = nil)
     hash = hash[0..5]
   end
   urldb = SQLite3::Database.open $dbfile
-  urldb.execute "CREATE TABLE IF NOT EXISTS urls(hashid varchar(20) primary key, url varchar(500))"
+  urldb.execute "CREATE TABLE IF NOT EXISTS urls(hash varchar(20) primary key, url varchar(500))"
   statement = urldb.prepare "INSERT INTO urls VALUES (?, ?)"
   statement.bind_param 1, dbstring(hash)
   statement.bind_param 2, dbstring(oldurl)
@@ -47,10 +47,11 @@ rescue SQLite3::ConstraintException => e
   # column hash is not unique
   # 1) URL already exists in the database and will hash to the same index
   # 2) someone already tried to use that postfix
+  # 3) by random chance a new URL hashed to an existing index
 
   # First, see if the postfix was set and is already in there
   if !postfix.empty?
-    statement = urldb.prepare "SELECT hashid FROM urls WHERE hashid = ?"
+    statement = urldb.prepare "SELECT hash FROM urls WHERE hashid = ?"
     statement.bind_param 1, dbstring(postfix)
     response = statement.execute
     row = response.next
@@ -58,9 +59,8 @@ rescue SQLite3::ConstraintException => e
     if !row.nil? # returned at least one row
       raise ArgumentError.new('That postfix has already been taken. Please use a different one or let me generate one.')
     end
-  # If not, then we must be seeing a duplicate URL that hashed to the same id.
-  else
-    puts "postfix nil, must be duplicate url"
+  elsif url_exists?(oldurl)
+    # URL already exists in the database, don't bother to generate a new one
     return hash
   end
 rescue SQLite3::Exception => e
@@ -75,4 +75,17 @@ def dbstring(s)
   else
     return s
   end
+end
+
+def url_exists?(url)
+  urldb = SQLite3::Database.open $dbfile
+  statement = urldb.prepare "SELECT hash FROM urls WHERE url = ?"
+  statement.bind_param 1, dbstring(url)
+  response = statement.execute
+  row = response.next
+  statement.close if statement
+  if !row.nil?
+    return true
+  end
+  return false
 end
