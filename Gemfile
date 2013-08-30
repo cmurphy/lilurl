018a25ed9a1403e0d9a3460f072c7d4232e332bf
Colleen Murphy <cmurphy@cat.pdx.edu>
2013-08-29 22:26:32 -0700

    Add ability to return JSON to a POST request.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sqlite3'
 gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'json'
--- lilurl.rb
+++ lilurl.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/contrib'
+require 'json'
 require './translate.rb'
 
 set :bind, '0.0.0.0'
@@ -30,9 +32,16 @@ post '/' do
     oldurl = params[:oldurl]
     postfix = params[:postfix]
     newurl=makeurl(oldurl, postfix)
-    erb :index, :locals => {:domain => request.host + port, :newurl => newurl, :error => nil}
+    newurl_hash = { 'newurl' => newurl }
+    respond_with :index do |format|
+      format.html { erb :index, :locals => {:domain => request.host + port, :newurl => newurl, :error => nil}}
+      format.json { "{newurl: http://#{request.host}#{port}/#{newurl}}" }
+    end
   rescue ArgumentError => e
-    erb :index, :locals => {:error => e.to_s }
+    respond_with :index do |format|
+      format.html { erb :index, :locals => {:error => e.to_s }}
+      format.json { "{error: #{e.to_s}}" }
+    end
   rescue SQLite3::Exception => e
     erb :index, :locals => {:error => "Database error: " + e.to_s }
   end
